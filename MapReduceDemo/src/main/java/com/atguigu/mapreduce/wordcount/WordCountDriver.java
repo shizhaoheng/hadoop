5ebff04754190efa0d8eab3ffe4549f059e3b433
shizhaoheng <mi@123456>
2021-03-16 21:57:38 +0800

    update

---
+++ MapReduceDemo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountDriver.java
@@ -0,0 +1,10 @@
+package com.atguigu.mapreduce.wordcount;
+
+/**
+ * @author : Zhaoheng Shi
+ * @date : 2021/3/16 20:54
+ */
+public class WordCountDriver  {
+
+
+}
---
+++ MapReduceDemo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountMapper.java
@@ -0,0 +1,28 @@
+package com.atguigu.mapreduce.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+/**
+ * @author : Zhaoheng Shi
+ * @date : 2021/3/16 20:54
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private Text outK = new Text();
+    private IntWritable outV = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        String line = value.toString();
+        String[] words = line.split(" ");
+        for (String word : words) {
+            outK.set(word);
+            context.write(outK, outV);
+        }
+
+    }
+}
