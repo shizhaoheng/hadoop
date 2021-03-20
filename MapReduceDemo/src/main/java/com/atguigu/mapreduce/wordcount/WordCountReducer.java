369b0b53726d185ba5a28ca54ae12277e400e31c
shizhaoheng <mi@123456>
2021-03-20 16:25:20 +0800

    update

--- MapReduceDemo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountReducer.java
+++ MapReduceDemo/src/main/java/com/atguigu/mapreduce/wordcount/WordCountReducer.java
@@ -1,8 +1,10 @@
 package com.atguigu.mapreduce.wordcount;
 
+import org.apache.hadoop.mapreduce.Reducer;
+
 /**
  * @author : Zhaoheng Shi
  * @date : 2021/3/16 20:54
  */
-public class WordCountReducer {
+public class WordCountReducer extends Reducer {
 }
